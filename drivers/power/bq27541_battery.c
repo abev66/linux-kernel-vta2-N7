17af4bc0d17f5396b17d922a1205fef982352070
motley <motley.slate@gmail.com>
2012-07-25 00:49:04 -0400

    Get rid of some kernel log spam

--- drivers/power/bq27541_battery.c
+++ drivers/power/bq27541_battery.c
@@ -65,7 +65,7 @@
 
 /* Debug Message */
 #define BAT_NOTICE(format, arg...)	\
-	printk(KERN_NOTICE "%s " format , __FUNCTION__ , ## arg)
+	pr_debug(KERN_NOTICE "%s " format , __FUNCTION__ , ## arg)
 
 #define BAT_ERR(format, arg...)		\
 	printk(KERN_ERR format , ## arg)
