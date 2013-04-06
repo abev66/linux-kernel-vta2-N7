4a51f67a59fc103570a502aaaa9c12c8595b26d0
lRabbit <abev66@gmail.com>
2013-04-06 14:35:34 +0800

    arm: tegra: cpu-tegra.c: prevent stress_test_enable error.

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -510,9 +510,9 @@ int tegra_update_cpu_speed(unsigned long rate)
 	for_each_online_cpu(freqs.cpu)
 		cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
-	if(stress_test_enable)
+/*	if(stress_test_enable)
 		printk(KERN_DEBUG "cpufreq-tegra: transition: %u --> %u\n",
-			freqs.old, freqs.new);
+			freqs.old, freqs.new);*/
 
 	ret = clk_set_rate(cpu_clk, freqs.new * 1000);
 	if (ret) {
